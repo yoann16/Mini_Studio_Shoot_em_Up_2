2ef2af371ed2105f83f44649945aa89e2b719912
Yoann <yoannjoseph16062000@gmail.com>
2025-03-19 19:38:28 +0100

    add Game.cpp

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -1,67 +1,97 @@
-//#include "Game.h"
-//#include "Ship.h"
-//#include "IGameObject.h"
-//#include <iostream>
-//
-//Game::Game(sf::RenderWindow* window, const float& framerate, TextureCache* texture) :
-//	ISceneBase(window, framerate, texture),
-//	cursor(this),
-//	m_bossSpawnTimer(10.0f)
+#include "Game.h"
+
+
+Game::Game(sf::RenderWindow* Window, const float& framerate, TextureCache* TextureManager)
+    :ISceneBase(Window, framerate, TextureManager)
+    , m_gameState(GameState::playing)
+{
+
+    m_backgroundLayer = new Layer(this, LayersType::Background,0);
+    m_middlegroundLayer = new Layer(this, LayersType::Middleground,1);
+    m_foregroundLayer = new Layer(this, LayersType::Foreground,2);
+
+    const sf::Vector2f windowCenter = sf::Vector2f(
+        Window->getSize().x / 2.0f,
+        Window->getSize().y / 2.0f
+    );
+    const sf::Vector2f treePosition =  sf::Vector2f(300.0f, 500.0f);
+
+    m_terrain = new Terrain(m_backgroundLayer, windowCenter);
+    m_player = new Player(m_middlegroundLayer, 10.0f, windowCenter);
+    m_treetrunk = new TreeTrunk(m_middlegroundLayer, treePosition);
+    m_treeleave = new TreeLeave(m_foregroundLayer, treePosition);
+    
+
+    m_backgroundLayer->AddGameObject(m_terrain);
+    m_middlegroundLayer->AddGameObject(m_player);
+    m_middlegroundLayer->AddGameObject(m_treetrunk);
+    m_foregroundLayer->AddGameObject(m_treeleave);
+
+
+}
+//Game::~Game()
 //{
-//	m_Background = new SquareSFML(10000, sf::Vector2f(0, 0));
-//	m_Background->setTexture(m_texture->getTexture("galaxie4.png"));
-//	m_ship = new Ship(this, m_Background);
-//	new BorderShip(m_ship, m_Background, static_cast<Ship*>(m_ship));
-//	new GameBorder(this, m_Background, Position::Down, 5);
-//	new GameBorder(this, m_Background, Position::Up, 5);
-//	new GameBorder(this, m_Background, Position::Left, 5);
-//	new GameBorder(this, m_Background, Position::Right, 5);
-//	new WorldBorder(this, m_Background, Position::Down, 5, 1000);
-//	new WorldBorder(this, m_Background, Position::Up, 5, 1000);
-//	new WorldBorder(this, m_Background, Position::Left, 5, 1000);
-//	new WorldBorder(this, m_Background, Position::Right, 5, 1000);
-//	m_spawner = new AsteroidSpawner(this, 10);
-//	m_bossSpawner = new BossSpawner(this, 1);
-//	sf::Vector2f bossSpawnPosition(GetCenterWindow().x, GetCenterWindow().y - 300);
-//	m_bossSpawner->SetSpawnPosition(bossSpawnPosition);
-//
-//	getWindow()->setMouseCursorVisible(false);
+//    delete m_backgroundLayer;
+//    delete m_middlegroundLayer;
+//    delete m_foregroundLayer;
+//    delete m_terrain;
+//    delete m_player;
 //}
-//
-//void Game::Update(const float& deltatime)
-//{
-//	for (auto& obj : getChildren())
-//	{
-//		obj->Update(deltatime);
-//	}
-//	cursor.Update(deltatime);
-//	m_spawner->Spawn();
-//
-//	m_bossSpawnTimer.NextTIck(deltatime);
-//	if (m_bossSpawnTimer.ActionIsReady()) {
-//		m_bossSpawner->Spawn();
-//		m_bossSpawnTimer.setNewTimer(9999999999999999.0f);
-//	}
-//
-//	auto vec = getFullTree();
-//	collision.HandleCollision(vec);
-//}
-//
-//void Game::ProssesInput(const sf::Event& event)
-//{
-//	for (auto& obj : getChildren())
-//	{
-//		obj->ProssesInput(event);
-//	}
-//	cursor.ProssesInput(event);
-//}
-//
-//void Game::Render()
-//{
-//	getWindow()->draw(static_cast<SquareSFML*>(m_Background)->getShape());
-//	for (auto& obj : getChildren())
-//	{
-//		obj->Render();
-//	}
-//	cursor.Render();
-//}
+void Game::ProcessInput(const sf::Event& event) 
+{
+    if (event.type == sf::Event::KeyPressed)
+    {
+        if (event.key.code == sf::Keyboard::P)
+        {
+            if (m_gameState == GameState::playing)
+                m_gameState = GameState::paused;
+            else if (m_gameState == GameState::paused)
+                m_gameState = GameState::playing;
+
+        }
+
+    }
+    m_player->ProcessInput(event);
+
+}
+void Game::Update(const float& deltatime) 
+{
+    //std::cout << "GameState: " << static_cast<int>(m_gameState) << std::endl;
+    switch (m_gameState)
+    {
+    case GameState::playing:
+    {
+
+        m_player->Update(deltatime);
+        break;
+
+    }
+    case GameState::paused:
+    {
+
+        //faire un truc
+        break;
+
+    }
+    case GameState::gameover:
+    {
+
+        //faire un truc
+        break;
+
+    }
+    }
+
+    m_backgroundLayer->Update(deltatime);
+    m_middlegroundLayer->Update(deltatime);
+    m_foregroundLayer->Update(deltatime);
+}
+void Game::Render() 
+{
+    m_backgroundLayer->Render();
+    m_middlegroundLayer->Render();
+    m_foregroundLayer->Render();
+    if (m_gameState == GameState::paused)
+    {
+    }
+}
