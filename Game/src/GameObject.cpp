a9735bba1b69bb706f34a376e045ceeb621a738f
Yoann <yoannjoseph16062000@gmail.com>
2025-04-01 10:30:43 +0200

    feature : ajout  de la Gestion des SpriteSheets
    Ayant des SpriteSheets de Taille Variable aussi bien en cellule dans une seule Sprite-Sheet  que des lignes et des colonnes j'ai décidé qu'on pouvait gérer ça avec des régions ou une grille standard si la Sprite-Sheet est un vrai tableau.

--- Game/src/GameObject.cpp
+++ Game/src/GameObject.cpp
@@ -12,5 +12,5 @@ void Player::ProcessInput(const sf::Event& event)
 void Player::Render()
 { }
 sf::Vector2f initPosition = { 0,0 };
-DEFINE_GAME_OBJECT(Player, initPosition, LayersType::Middleground, 0b1101)
+DEFINE_GAME_OBJECT(Player, initPosition, LayersType::Middleground, 0b1011)
 //DEFINE_GAME_OBJECT(Player,Position,1,0b1101)
